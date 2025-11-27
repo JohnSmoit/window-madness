eb1ae060cf76f1696d8143adaf6c9a470c137e89
JohnSmoit <ju.smoit@gmail.com>
2025-11-26 21:14:14 -0800

    feat(x11): Implemented a barebones runtime loader for vulkan  functions both instanced scoped and otherwise

--- zig/vk_x11.zig
+++ zig/vk_x11.zig
@@ -1,6 +1,7 @@
 //! What essentially maps to a direct c translation of a basic X11 window setup
 //! for reference with vulkan compatibility included in initialization paramereters
 const std = @import("std");
+const vk = @import("vulkan");
 
 const x11 = @cImport({
     @cInclude("X11/Xlib.h");
@@ -34,6 +35,59 @@ const WindowParams = struct {
     win_attrs:      x11.XSetWindowAttributes,
 };
 
+/// A very barebones loader for linux based platforms...
+/// Certainly doesn't cover all the cases and possibilities but works well enough
+/// as an example
+const BasicVulkanLoader = struct {
+    var vk_lib: ?std.DynLib = null;
+    var getInstanceProc: ?vk.PfnGetInstanceProcAddr = null;
+
+    fn load(instance: vk.Instance, name: [*:0]const u8) ?*anyopaque {
+        if (vk_lib == null) {
+            //TODO: Find a better way to control search paths for dll loaders
+            const search_paths: []const [:0]const u8 = &.{
+                "libvulkan.so",
+                "/usr/lib64/libvulkan.so",
+                "/usr/lib/libvulkan.so",
+            }; 
+
+            for (search_paths) |path| {
+                vk_lib = std.DynLib.open(path) catch 
+                    continue;
+                break;
+            }
+
+            if (vk_lib == null) 
+                @panic("Failed to load vulkan library. Ensure it is loaded and check search paths");
+        }
+
+        const name_buf: [:0]const u8 = std.mem.span(name);
+
+        if (instance == .null_handle) {
+            const func = vk_lib.?.lookup(*anyopaque, name_buf) orelse {
+                std.debug.print("Failed to load function: {s}\n", .{name_buf});
+                return null;
+            };  
+
+            if (getInstanceProc == null and std.mem.eql(u8, name_buf, "vkGetInstanceProcAddr")) {
+                getInstanceProc = @ptrCast(@alignCast(func));
+            }
+
+            return func;
+        } else {
+            std.debug.assert(getInstanceProc != null);
+            std.debug.assert(vk_lib != null);
+
+            return getInstanceProc.?(instance, name);
+        }
+    }
+};
+
+var vk_instance: vk.Instance = .null_handle;
+
+var inst_proxy: vk.InstanceProxy = undefined;
+var base_wrapper: vk.BaseWrapper = undefined;
+
 pub fn main() !void {
     // a Display in X is a set of "screens" for a single user
     const main_display = x11.XOpenDisplay(0);
@@ -41,9 +95,9 @@ pub fn main() !void {
 
     // Top level  window in a windowing heirarchy, which covers the entirety
     // of a screen.
-    
     const root_window  = x11.XDefaultRootWindow(main_display);
 
+    std.debug.print("address of function: {*}\n", .{&x11.XOpenDisplay});
     
     // a Screen in X represents a physical monitor/hardware systems
     // In my case, there is 1 screen.
@@ -53,7 +107,6 @@ pub fn main() !void {
     // to Win32's nasty GDI.
     // Just for reference -- vulkan is the only nightmare API I need
     // const gc_context   = x11.XDefaultGC(main_display, screen);
-    
     var wp           = WindowParams{
         .x = 0,
         .y = 0,
@@ -73,7 +126,6 @@ pub fn main() !void {
         }
     };
 
-    
     const window       = x11.XCreateWindow(
         main_display,
         root_window,
@@ -88,25 +140,21 @@ pub fn main() !void {
         wp.attr_val_mask,
         &wp.win_attrs,
     );
-    
 
     // map the window into the display heirarchy, with heirarchal position as specified
     // in XCreatWindow.
     _ = x11.XMapWindow(main_display, window);
 
     _ = x11.XStoreName(main_display, window, "Vulkan from X11 surface");
-    
 
     // this is how the window server X communicates between clients, which includes window managers.
     // In this case, we retrieve the atom for the server's window destructor signal.
     // This way, our window will recieve and understand delete signals sent from other clients.
     // Apparently, this isn't strictly required, guh.
-    var wm_delete_window = x11.XInternAtom(main_display, "WM_DELETE_WINDOW", x11.False);
-    
-    if (x11.XSetWMProtocols(main_display, window, &wm_delete_window, 1) == 0) {
+    const wm_delete_window = x11.XInternAtom(main_display, "WM_DELETE_WINDOW", x11.False);
+    if (x11.XSetWMProtocols(main_display, window, wm_delete_window, 1) == 0) {
         std.debug.print("Error: Failed to register window manager's delete property!\n", .{});
     }
-    
 
     while (true) {
         var event: x11.XEvent = undefined;
