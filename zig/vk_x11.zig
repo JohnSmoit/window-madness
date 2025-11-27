6198b8ff393e196b4667d6c4e1762faad8678e83
JohnSmoit <ju.smoit@gmail.com>
2025-11-26 22:05:04 -0800

    feat(x11): slightly more rawdogged then usual vulkan context initialization

--- zig/vk_x11.zig
+++ zig/vk_x11.zig
@@ -42,7 +42,7 @@ const BasicVulkanLoader = struct {
     var vk_lib: ?std.DynLib = null;
     var getInstanceProc: ?vk.PfnGetInstanceProcAddr = null;
 
-    fn load(instance: vk.Instance, name: [*:0]const u8) ?*anyopaque {
+    pub fn load(instance: vk.Instance, name: [*:0]const u8) vk.PfnVoidFunction {
         if (vk_lib == null) {
             //TODO: Find a better way to control search paths for dll loaders
             const search_paths: []const [:0]const u8 = &.{
@@ -64,7 +64,7 @@ const BasicVulkanLoader = struct {
         const name_buf: [:0]const u8 = std.mem.span(name);
 
         if (instance == .null_handle) {
-            const func = vk_lib.?.lookup(*anyopaque, name_buf) orelse {
+            const func = vk_lib.?.lookup(vk.PfnVoidFunction, name_buf) orelse {
                 std.debug.print("Failed to load function: {s}\n", .{name_buf});
                 return null;
             };  
@@ -81,12 +81,97 @@ const BasicVulkanLoader = struct {
             return getInstanceProc.?(instance, name);
         }
     }
+
+    pub fn cleanup() void {
+        if (vk_lib) |*lib| {
+            lib.close();
+        }
+    }
 };
 
 var vk_instance: vk.Instance = .null_handle;
 
 var inst_proxy: vk.InstanceProxy = undefined;
+var inst_wrapper: vk.InstanceWrapper = undefined;
+var dbg_messenger: vk.DebugUtilsMessengerEXT = .null_handle;
 var base_wrapper: vk.BaseWrapper = undefined;
+var surf: vk.SurfaceKHR = .null_handle;
+
+fn dbai(b: vk.DebugUtilsMessageSeverityFlagsEXT) u32 {
+    return @bitCast(b);
+}
+
+fn debugCallback(
+    flags: vk.DebugUtilsMessageSeverityFlagsEXT,
+    msg_type: vk.DebugUtilsMessageTypeFlagsEXT,
+    data: ?*const vk.DebugUtilsMessengerCallbackDataEXT,
+    user_data: ?*anyopaque,
+) callconv(.c) vk.Bool32 {
+    const logger = std.log.scoped(.vulkan);
+    if (data) |dat| {
+        const msg = dat.p_message orelse "(no message provided)";
+        switch (dbai(flags)) {
+            dbai(.{.info_bit_ext = true}) => logger.info("{s}", .{msg}),
+            dbai(.{.warning_bit_ext = true}) => logger.warn("{s}", .{msg}),
+            dbai(.{.error_bit_ext = true}) => logger.err("{s}", .{msg}),
+            dbai(.{.verbose_bit_ext = true}) => logger.debug("{s}", .{msg}),
+            else => logger.debug("Oh fuck, something reaaaaaaaaaaaal bbad haappeened aaaaah", .{}),
+        }
+    } else {
+        logger.err("An unknown error occured", .{});
+    }
+
+    _ = user_data;
+    _ = msg_type;
+
+    return .true;
+}
+
+fn initVulkanSurface() !void {
+    base_wrapper = vk.BaseWrapper.load(BasicVulkanLoader.load);
+    
+    const extensions: []const [*:0]const u8 = &.{
+        vk.extensions.khr_surface.name.ptr,
+        vk.extensions.ext_debug_utils.name.ptr,
+    };
+
+    const layers: []const [*:0]const u8 = &.{
+        "VK_LAYER_KHRONOS_validation",
+    };
+
+    vk_instance = try base_wrapper.createInstance(&vk.InstanceCreateInfo{
+        .enabled_extension_count = @intCast(extensions.len),
+        .pp_enabled_extension_names = extensions.ptr,
+        .enabled_layer_count = @intCast(layers.len),
+        .pp_enabled_layer_names = layers.ptr,
+        .p_application_info = &vk.ApplicationInfo{
+            .api_version = @bitCast(vk.makeApiVersion(0, 69, 42, 67)),
+            .application_version = 0,
+            .engine_version = 0,
+            .p_application_name = "Partial J*b Application",
+            .p_engine_name = "Larry Engine",
+        },
+        .p_next = &vk.DebugUtilsMessengerCreateInfoEXT{
+            .message_severity = .{
+                .info_bit_ext = true,
+                .warning_bit_ext = true,
+                .error_bit_ext = true,
+                .verbose_bit_ext = true,
+            },
+            .message_type = .{
+                .validation_bit_ext = true,
+                .general_bit_ext = true,
+            },
+            .pfn_user_callback =  debugCallback,
+        },
+    }, null);
+    defer inst_proxy.destroyInstance(null);
+
+    inst_wrapper = vk.InstanceWrapper.load(vk_instance,BasicVulkanLoader.load);
+    inst_proxy = vk.InstanceProxy.init(vk_instance, &inst_wrapper);
+
+    std.debug.print("If you're seeing this vulkan sort of worked at least a little bit!\n", .{});
+}
 
 pub fn main() !void {
     // a Display in X is a set of "screens" for a single user
@@ -151,10 +236,13 @@ pub fn main() !void {
     // In this case, we retrieve the atom for the server's window destructor signal.
     // This way, our window will recieve and understand delete signals sent from other clients.
     // Apparently, this isn't strictly required, guh.
-    const wm_delete_window = x11.XInternAtom(main_display, "WM_DELETE_WINDOW", x11.False);
-    if (x11.XSetWMProtocols(main_display, window, wm_delete_window, 1) == 0) {
+    var wm_delete_window = x11.XInternAtom(main_display, "WM_DELETE_WINDOW", x11.False);
+    if (x11.XSetWMProtocols(main_display, window, &wm_delete_window, 1) == 0) {
         std.debug.print("Error: Failed to register window manager's delete property!\n", .{});
     }
+
+    try initVulkanSurface();
+    defer BasicVulkanLoader.cleanup();
 
     while (true) {
         var event: x11.XEvent = undefined;
